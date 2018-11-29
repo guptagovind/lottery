ada708cfe13250dd67baab713fb0cd8b20efea52
govind <govind.gupta@innoplexus.com>
2018-11-29 07:41:47 +0530

    Lottery App: Completion of lottery app using ethereum contract.

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3'
+
+const address = '0xe09504459510777b57c40B68F0e213E7B4c49d3d';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
